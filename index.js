8e2c0f0261dd6d287072e741aec1293d8e4a7890
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-05 10:20:33 +0530

    initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://souvik:souvik@cluster0.bbuqkww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log("Failed", err);
+  });
+
+const app = express();
+
+app.listen(8086, () => {
+  console.log("Server started at port 8086");
+});
