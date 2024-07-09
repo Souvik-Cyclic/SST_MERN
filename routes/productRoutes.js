87d2b173b7fcf2e3916eb112cced458e795a02df
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-09 18:37:10 +0530

    code refactoring

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,42 +1,17 @@
 const express = require("express");
-
 const router = require("express").Router();
+
 // post
-router.post("/api/products", async (req, res) => {
-  productModel.create({
-    product_name: req.body.product_name,
-    product_price: req.body.product_price,
-    isInStock: req.body.isInStock,
-    category: req.body.category,
-  });
-  return res.status(201).json({ message: "Product Created" });
-});
+router.post("/api/products", );
 
 // get
-router.get("/api/products", async (req, res) => {
-  const allProducts = await productModel.find({});
-
-  return res.json(allProducts);
-});
+router.get("/api/products");
 
 // get product by id
-router.get("/api/products/:id", async (req, res) => {
-  const product = await productModel.findById(req.params.id);
-
-  return res.json(product);
-});
+router.get("/api/products/:id");
 
 // update product
-router.put("/api/products/:id", async (req, res) => {
-  const updatedProduct = await productModel.findByIdAndUpdate(
-    req.params.id,
-    req.body
-  );
-  return res.json(updatedProduct);
-});
+router.put("/api/products/:id");
 
 // delete
-router.delete("/api/products/:id", async (req, res) => {
-  const deletedProduct = await productModel.findByIdAndDelete(req.params.id);
-  res.json({ message: "Product Deleted", deletedProduct });
-});
+router.delete("/api/products/:id");
